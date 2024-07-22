ede05bdf3ccb5586d67187b311a7f8fcea05e9f0
Mark Andreev <mark.andreev@gmail.com>
2024-07-23 00:26:11 +0100

    Add example for spark 3.1.2 & 4.0.0

---
+++ spark_4.0.0/build.sbt
@@ -0,0 +1,18 @@
+name := "code"
+
+version := "0.1"
+
+scalaVersion := "2.13.14"
+
+lazy val sparkVersion = "4.0.0-preview1"
+lazy val typeSafeConfigVersion = "1.4.1"
+lazy val scalatestVersion = "3.2.3"
+
+libraryDependencies += "com.typesafe" % "config" % typeSafeConfigVersion
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % sparkVersion % Provided
+libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion % Provided
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % sparkVersion % Test
+libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion % Test
+libraryDependencies += "org.scalatest" %% "scalatest" % scalatestVersion % Test
---
+++ spark_4.0.0/src/test/scala/ErrorMsgSuite.scala
@@ -0,0 +1,45 @@
+import org.apache.spark.SparkException
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{BinaryType, LongType, StringType, StructType}
+import org.scalatest.funsuite.AnyFunSuite
+
+
+class ErrorMsgSuite extends AnyFunSuite with SharedSparkContext {
+  test("shouldThrowSchemaError") {
+    val seq: Seq[Row] = Seq(
+      Row(
+        toBytes("0"),
+        toBytes(""),
+        1L,
+      ),
+      Row(
+        toBytes("0"),
+        toBytes(""),
+        1L,
+      ),
+    )
+
+    val schema: StructType = new StructType()
+      .add("f1", BinaryType)
+      .add("f3", StringType)
+      .add("f2", LongType)
+
+    val df = sqlContext.createDataFrame(sqlContext.sparkContext.parallelize(seq), schema)
+
+    val exception = intercept[SparkException] {
+      df.show()
+    }
+
+    assert(
+      exception.getCause.getCause.getMessage
+        .contains("[B is not a valid external type for schema of string")
+    )
+    assertResult(
+      "[B is not a valid external type for schema of string at getexternalrowfield(assertnotnull(input[0, org.apache.spark.sql.Row, true]), 1, f3)"
+    )(exception.getCause.getCause.getMessage)
+  }
+
+
+
+  def toBytes(x: String): Array[Byte] = x.toCharArray.map(_.toByte)
+}
